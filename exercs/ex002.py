b8297365b28937b3d06c5ea2a2d3258084999fa8
gugamacedo <81040021+gugamacedo@users.noreply.github.com>
2021-03-22 15:52:29 -0300

    Exerc 2

---
+++ exercs/ex002.py
@@ -0,0 +1,5 @@
+# Crie um script que leia o dia, mês e ano de nascimento de uma pessoa e mostre uma mensagem com a dta formatada
+dianasc = input('Dia de nascimento: ')
+mesnasc = input('Agora o mês: ')
+anonasc = input('Por fim o ano: ')
+print(f'Você nasceu em {dianasc} de {mesnasc} de {anonasc}, correto?')
