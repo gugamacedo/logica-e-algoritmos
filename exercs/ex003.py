176c25fddbcd6909d1c5dd9db66243f1a98c0871
gugamacedo <81040021+gugamacedo@users.noreply.github.com>
2021-03-22 15:53:09 -0300

    Exerc 3

---
+++ exercs/ex003.py
@@ -0,0 +1,4 @@
+# Crie um script que leia dois números e tente mostrar a soma entre eles
+num01 = int(input('Vamos somar, me diga um número qualquer! '))
+num02 = int(input('Agora outro número! '))
+print(f'A soma entre {num01} e {num02} é: {num01 + num02}')
