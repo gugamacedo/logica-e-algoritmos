a0a05c6f79fdc88806dcc70c775605c86ba0ffbd
gugamacedo <81040021+gugamacedo@users.noreply.github.com>
2021-03-22 15:55:49 -0300

    Exerc 5

---
+++ exercs/ex005.py
@@ -0,0 +1,3 @@
+# Faça um programa que leia um número inteiro e mostre seu antecessor e sucessor
+num01 = int(input('Diz um número: '))
+print(f'O antecessor de {num01} é {num01-1}\nO sucessor de {num01} é {num01+1}')
