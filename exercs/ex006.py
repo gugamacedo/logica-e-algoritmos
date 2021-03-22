cffe41b38dfed37d9180c1a36cf720503672a5b5
gugamacedo <81040021+gugamacedo@users.noreply.github.com>
2021-03-22 15:55:55 -0300

    Exerc 6

---
+++ exercs/ex006.py
@@ -0,0 +1,5 @@
+# Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada
+num01 = int(input('Diz um número: '))
+print(f'A raiz quadrada de {num01} é {num01**(1/2):.2f}\n'
+      f'A raiz cúbica de {num01} é {num01**(1/3):.2f}\n'
+      f'A raiz quadratíca de {num01} é {num01**(1/4):.2f}')
