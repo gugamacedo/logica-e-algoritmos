ff693f769238c29c2a0ed29d930a3c7001e539ca
gugamacedo <81040021+gugamacedo@users.noreply.github.com>
2021-03-22 15:52:51 -0300

    Aula 6

---
+++ aulas/aula06.py
@@ -0,0 +1,21 @@
+'''
+- Tipos primitivos:
+- int números inteiros
+- 7, -4, 0, 99898
+- float números reais, também chamados de pontos flutuantes
+- 7.0, -4.5, 0.076, -15.223
+- Python e o padrão internacional utiliza ponto ao invés da vírgula em números quebrados
+- bool valores lógicos ou booleanos
+- True ou False
+- str valores caractere ou string
+- ‘Olá’, ‘7.5’, ‘’
+
+print(f’{variavel:.2f}’) para dizer em quantos caracteres deve conter a variável
+(Pode colocar um f depois do número de caracteres, para definir a quantidade após a vírgula)
+
+\n pula linha
+end=’’ para dizer o que vem no final da linha, pode ser vazio, espaço, qualquer coisa
+
+Para calcular raiz quadrada só fazer a potenciação em fração entre parênteses
+4**(1/2)
+'''
