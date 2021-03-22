f79e1563c8efd5db3fb408b93cfe2f5be8d8c787
gugamacedo <81040021+gugamacedo@users.noreply.github.com>
2021-03-22 15:56:23 -0300

    Exerc 8

---
+++ exercs/ex008.py
@@ -0,0 +1,3 @@
+# Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e mílimetros
+metr = int(input('Diga quantos metros quer converter para centímetros: '))
+print(f'{metr}m em centímetros é {metr*100}cm e {metr*1000} em mílimetros')
