4d3d126dfd24ebb68f5777132507ccd7b2383ce8
gugamacedo <81040021+gugamacedo@users.noreply.github.com>
2021-03-23 16:35:25 -0300

    Exerc 10

---
+++ exercs/ex010.py
@@ -0,0 +1,3 @@
+# Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar
+dindin = float(input('Diga quantos reais quer converter pra doleta: '))
+print(f'Você têm U${dindin / 5.67:.2f} dólares')
