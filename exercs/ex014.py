a7e1519ea8a3c45cea1cd55e6d98c8ce76075d20
gugamacedo <81040021+gugamacedo@users.noreply.github.com>
2021-03-23 16:35:56 -0300

    Exerc 14

---
+++ exercs/ex014.py
@@ -0,0 +1,4 @@
+# Converta Celsius para Fahrenheit
+temp = float(input('Digite a temperatura: '))
+# (30 °C × 9/5) + 32 = 86 °F
+print(f'A temperatura {temp}°, em Celsius, corresponde a {temp * 1.8 + 32}° Fahrenheit')
