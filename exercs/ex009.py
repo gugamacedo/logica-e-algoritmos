0d3f899db3432f41a0a1e61346835df3f2b727c2
gugamacedo <81040021+gugamacedo@users.noreply.github.com>
2021-03-23 16:35:17 -0300

    Exerc 9

---
+++ exercs/ex009.py
@@ -0,0 +1,12 @@
+# Faça um programa que leia um número inteiro qualquer e mostre na tela sua tabuada
+num = int(input('Diga o número e te mostratei a tabuada dele: '))
+print(f'{num}*1={num * 1}\n'
+      f'{num}*2={num * 2}\n'
+      f'{num}*3={num * 3}\n'
+      f'{num}*4={num * 4}\n'
+      f'{num}*5={num * 5}\n'
+      f'{num}*6={num * 6}\n'
+      f'{num}*7={num * 7}\n'
+      f'{num}*8={num * 8}\n'
+      f'{num}*9={num * 9}\n'
+      f'{num}*10={num * 10}\n')
