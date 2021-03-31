c473b7f847740468b53ab9effdbe6edddf3bf8e4
gugamacedo <81040021+gugamacedo@users.noreply.github.com>
2021-03-31 13:29:28 -0300

    Condições Aninhadas

---
+++ aulas/aula12.py
@@ -0,0 +1,17 @@
+'''
+Condições Aninhadas - ELIF
+Quando você coloca uma coisa dentro da outra, uma condição dentro da outra
+O condicional normal ou é if ou else, já na condição aninhada você lida com vários caminhos
+
+Sò se usa um if, um else, mas ELIF pode usar vários
+Só se usa elif quando tem if
+
+nome = str(input('Qual seu nome? '))
+if nome == 'Joasney':
+    print(f'Eae, {nome}, nome bonito hein!')
+elif nome == 'Maricreusa':
+    print(f'Eae, {nome}, nome bonito hein!')
+else:
+    print(f'{nome}...nome zoado hein')
+
+'''
