4d3d475cbe632363fa882472478a1bb63268f636
gugamacedo <81040021+gugamacedo@users.noreply.github.com>
2021-04-08 13:40:47 -0300

    Tabuada v2

---
+++ exercs/ex049.py
@@ -0,0 +1,6 @@
+# Atutomatize o exer 9 com laço For
+num = int(input('Diga o número e te mostrarei a tabuada dele: '))
+# stock = 0
+for count in range(1, 11):
+    print(f'{num} x {count} = {num*count}')
+    # stock += 1
