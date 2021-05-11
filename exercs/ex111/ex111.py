dcdeab36bfda0314d8eddcb03c638b6c608c9f8b
gugamacedo <81040021+gugamacedo@users.noreply.github.com>
2021-05-11 15:34:03 -0300

    111 | Transformando modulos em pacotes

---
+++ exercs/ex111/ex111.py
@@ -0,0 +1,6 @@
+# Adicione na moeda.py uma função resumo() que vai trazer tudo dos prints, mas numa função
+from utilidadescev import moeda
+
+valor = float(input('Digite um valor: R$'))
+
+moeda.resumo(valor, 20, 10)
